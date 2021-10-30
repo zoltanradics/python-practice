73b4e1774adc09d0ea0eb6c8b894174822e1bdc3
zoltanradics <zoltan.radics@gmail.com>
2021-10-30 15:18:22 +0200

    Coments added

--- concat_string.py
+++ concat_string.py
@@ -1,8 +1,11 @@
 
 def concat_string(*args):
+  # Define string to return
   return_string = ''
 
+  # Convert any given argument to string and concat them together
   for arg in args:
     return_string += str(arg)
-    
+  
+  # Return string
   return return_string
