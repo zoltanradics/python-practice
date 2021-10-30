b7c90c0abd333cf91fc94ae77e165369108bacc3
zoltanradics <zoltan.radics@gmail.com>
2021-10-30 15:15:07 +0200

    Introduce index file

---
+++ index.py
@@ -0,0 +1,11 @@
+from concat_string import concat_string
+from add_prefix import add_prefix
+from remove_suffix import remove_suffix
+
+# Add prefix
+print('add_prefix: ', add_prefix('auto', 'gram'))
+print('add_prefix: ', add_prefix('auto', ['gram', 'motive', 'matic']))
+
+# Remove suffix
+print('remove_suffix: ', remove_suffix('happiness', 'ness'))
+print('remove_suffix: ', remove_suffix('smartness', 'ness'))
